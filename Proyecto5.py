80ce1b7059c4cf1f440794368f6401dba19b6f86
Oscar CENTENO MORA <oscarcenteno86@gmail.com>
2023-06-08 09:02:57 -0600

    Add ocm2
    Agregué mi parte del proyecto

--- Proyecto5.py
+++ Proyecto5.py
@@ -1,7 +1,16 @@
 # Proyecto 5 Pull Request
 
-import pandas as pd
+##########################
+#   Módulos 
+##########################
 
+import pandas as pd
+import numpy as np 
+
+#############################
+#  Datos iniciales Jenny    #
+#############################
+ 
 data = {
     'Nombre de la película': ['El Padrino', 'Titanic', 'El Señor de los Anillos', 'Avatar', 'Harry Potter y la Piedra Filosofal'],
     'Productor': ['Francis Ford Coppola', 'James Cameron', 'Peter Jackson', 'James Cameron', 'David Heyman'],
@@ -12,7 +21,10 @@ df = pd.DataFrame(data)
 print(df)
 
 
-#Aporte OCM
+#############################
+#  Aporte  OCM 1            #
+#############################
+
 nuevas_observaciones = {
     'Nombre de la película': ['rapidos y furiosos'],
     'Productor': ['don Juan'],
@@ -23,5 +35,23 @@ df_nuevas_observaciones = pd.DataFrame(nuevas_observaciones)
 
 # Agregar las nuevas observaciones al DataFrame existente
 df = df.append(df_nuevas_observaciones, ignore_index=True)
+
+print(df)
+
+#############################
+#  Aporte  OCM 2            #
+#############################
+
+df_draft = nuevas_observaciones = {
+    'Nombre de la película': ['Avatar 2: el camino del agua'],
+    'Productor': ['James Cameron'],
+    'Género': ['Ciencia Fic.']
+}
+
+df_OCM2 = pd.DataFrame(df_draft)
+
+# Agregar las nuevas observaciones al DataFrame existente
+
+df = df.append(df_OCM2, ignore_index=True)
 
 print(df)
